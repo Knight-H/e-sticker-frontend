0053a61cd0370a7358f3302d6a23d4230a28fbdb
Knight H <thanapapas@hotmail.com>
2020-09-20 20:34:53 +0000

    add before merge

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { Route, Switch } from "react-router-dom";
 
 import NavBarComponent from "./components/navbar";
 import HomeComponent from "./components/home";
+import Order1ProductConfigComponent from "./components/order-1-product-config"
+
+
 
 function App() {
     return (
@@ -10,6 +13,7 @@ function App() {
             <NavBarComponent />
             <Switch>
                 <Route path="/" exact component={HomeComponent} />
+                <Route path="/order-1-product-config" component={Order1ProductConfigComponent} />
             </Switch>
         </div>
     );
