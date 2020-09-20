de39ea13470dbf5ca88855da22b6c6edcdb00ffc
codenuk <supagorn00001@gmail.com>
2020-09-20 10:01:57 +0000

    update comment K.Knight at 18-Sep

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,26 +1,37 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { withRouter } from 'react-router';
 import styles from './index.module.scss';
 
-const FooterComponent = ({ history }) => {
+import { ReactComponent as BannerFooter } from './banner-footer.svg';
+import { ReactComponent as QrCode } from './qr-code.svg';
+
+const FooterComponent = () => {
     return (
         <footer>
-            <div className={styles.bg_footer}>
+            <div className={styles.bgFooter}>
+                <BannerFooter  />
                 <ul>
-                    <li className={styles.title_bold}>เมนู</li>
+                    <li><b>เมนู</b></li>
                     <li>สั่งสติกเกอร์</li>
                     <li>ขั้นตอนการสั่ง</li>
                     <li>ดูสถานะการสั่งซื้อ</li>
                     <li>ติดต่อเรา</li>
                 </ul>
-
+                <ul className={styles.colQrCode}>
+                    <li><b>Scan Line QR code</b></li>
+                    <li><QrCode /></li>
+                </ul>
+                <ul className={styles.lisrContract}>
+                    <li className={styles.squareLine}><b>Line Official</b><span className={styles.squareWhite}>@digitalwish.sticker</span></li>
+                    <li className={styles.squareFace}><b>Facebook</b><span className={styles.squareWhite}>digitalwish.sticker</span></li>
+                    <li className={styles.squareEmail}><b>Email</b><span className={styles.squareWhite}>contact@stickerwish.com</span></li>
+                </ul>
             </div>
-            <div className={styles.footer}>
+            <div className={styles.footerBar}>
                 © 2020 Digitalwish Sticker by Digitalwish CO. LTD
             </div>
         </footer>
     );
 };
 
-export default withRouter(FooterComponent);
+export default FooterComponent;
