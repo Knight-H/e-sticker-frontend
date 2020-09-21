4ab19a3e0f46658f227778de479527e028b8da72
codenuk <supagorn00001@gmail.com>
2020-09-21 17:26:34 +0000

    update page approve but not responsive

--- src/components/approve-layout/index.js
+++ src/components/approve-layout/index.js
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import styles from './index.module.scss';
 import StepProgress from "../step_progress";
+import Kerry from './kerry.png';
+import Scb from './scb.jpg';
+import img_product from '../shopping/workplace.jpg';
 
 import { ReactComponent as Circle } from './circle.svg';
 import { ReactComponent as Drawing } from './drawing.svg';
@@ -13,7 +16,7 @@ const ApproveLayoutComponent = () => {
             <h3>ออเดอร์หมายเลข #DW0001 <label className={styles.waitApproval}>กำลังดำเนินการ</label></h3>
 
             <section className={styles.stepProgressBar}>
-                <StepProgress stepIndex={selectStep}/>
+                <StepProgress stepIndex={selectStep} />
             </section>
 
             <section className={styles.containCard}>
@@ -67,6 +70,73 @@ const ApproveLayoutComponent = () => {
                     </div>
                 </div>
             </section>
+
+            <section className={styles.groupDeliveryPayment}>
+                <div className={styles.groupDelivery}>
+                    <h3><b>การจัดส่ง</b></h3>
+                    <div className={styles.containInformationCustomer}>
+                        <p>นายลูกค้า สติกเกอร์</p>
+                        <p>110 ซอย สาธุประดิษฐ์ 58
+                        แขวง บางโพงพาง เขต ยานนาวา
+                        กรุงเทพมหานคร 10120
+                        </p>
+                        <p>เบอร์โทรศัพท์: 0900000000</p>
+                        <p>อีเมล: contact@gmail.com</p>
+                    </div>
+
+                    <div className={styles.containInformation}>
+                        <h4>จัดส่งโดย</h4>
+                        <img src={Kerry} className={styles.deliveryBy} alt="kerry" />
+                        <br />
+                        <label>เลข Tracking: รอการจัดส่ง</label>
+                        <label>ติดตาม</label>
+                    </div>
+                </div>
+
+                <div className={styles.groupPayment}>
+                    <h3><b>การชำระเงิน</b></h3>
+                    <label><img src={Scb} className={styles.logoBank} width="25" alt="kerry" />ธนาคารไทยพาณิชย์</label>
+
+                    <table>
+                        <thead>
+                            <tr>
+                                <th colspan="2" style={{ textAlign: "left" }}>สินค้า</th>
+                                <th className={styles.textRight}>จำนวน</th>
+                                <th className={styles.textRight}>มูลค่า</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td><img src={img_product} alt="Product" /></td>
+                                <td>
+                                    <p>สติกเกอร์แบบกลม</p>
+                                    <span>กระดาษอาร์ต - เคลือบด้าน -กินเนื้อ 1 มม. - ขนาด 10x20 mm </span>
+                                </td>
+                                <td className={styles.textRight}>300</td>
+                                <td className={styles.textRight}>500฿</td>
+                            </tr>
+                        </tbody>
+                        <tbody>
+                            <tr>
+                                <td colspan="3" style={{ borderTop: "1px solid black" }}>ค่าสินค้ารวม</td>
+                                <td className={styles.textRight} style={{ borderTop: "1px solid black" }}>1000฿</td>
+                            </tr>
+                            <tr>
+                                <td colspan="3" style={{ borderBottom: "1px solid black" }}>ค่าจัดส่ง</td>
+                                <td className={styles.textRight} style={{ borderBottom: "1px solid black" }}>50฿</td>
+                            </tr>
+                        </tbody>
+                        <tfoot>
+                            <tr>
+                                <td colspan="3">รวมทั้งหมด</td>
+                                <td className={styles.textRight}>1050฿</td>
+                            </tr>
+                        </tfoot>
+                    </table>
+
+                </div>
+            </section>
+
         </main >
     );
 };
