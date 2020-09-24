6180e385d50837569a5d32ab09e89a3fca007389
codenuk <supagorn00001@gmail.com>
2020-09-24 19:01:10 +0000

    edit warning, edit css

--- src/components/upload-file/index.js
+++ src/components/upload-file/index.js
@@ -1,21 +1,21 @@
 import React, { useState } from "react";
 import styles from './index.module.scss';
-// import stylesDropDawn from './drop-dawn.module.scss';
 import { Link } from "react-router-dom";
 
 import StepProgress from "../step_progress";
-import '../../fontawesome-free/css/all.css';
 import { ReactComponent as IconUploadFile } from './icon-upload-file.svg';
 import { ReactComponent as IconCart } from './icon-cart.svg';
 import { ReactComponent as IconArrow } from './icon-arrow.svg';
+import { ReactComponent as IconCheckSVG } from './icon-check.svg';
 
+import IconCheck from './icon-check.png';
 const UploadFileComponent = () => {
     const [file, setFile] = useState("");
     const [selectStep] = useState(2);
     const [dropDawn, setDropDawn] = useState(0);
 
     const handleChangeDropDawn = (e) => {
-        console.log("e>>>", e.value)
+        // console.log("e>>>", e.value)
         setDropDawn(e.value);
     };
 
@@ -31,7 +31,7 @@ const UploadFileComponent = () => {
                 <StepProgress stepIndex={selectStep} />
             </section>
             <section className={styles.wrapContent}>
-                <img src={file} className={styles.square} />
+                <img src={file} className={styles.square} alt="."/>
 
                 <div className={styles.rightContent}>
                     <label className={styles.label}>อนุมัติรูปแบบงาน</label>
@@ -39,12 +39,12 @@ const UploadFileComponent = () => {
                     <div className={styles.selectBox}>
                         <div className={styles.selectBoxCurrent} tabindex="1">
                             <div className={styles.selectBoxValue}>
-                                <input className={styles.selectBoxInput} type="radio" id="1" value="1" checked={dropDawn == 1 ? true : false}
+                                <input className={styles.selectBoxInput} type="radio" id="1" value="1" checked={`${dropDawn}` === `${1}` ? true : false}
                                     onChange={(e) => handleChangeDropDawn(e.target)} />
-                                <p className={styles.selectBoxInputText}><i className="fas fa-check"></i>ต้องการอนุมัติ</p>
+                                <p className={styles.selectBoxInputText}><IconCheckSVG className={styles.positionIcon}/>ต้องการอนุมัติ</p>
                             </div>
                             <div className={styles.selectBoxValue}>
-                                <input className={styles.selectBoxInput} type="radio" id="0" value="0" checked={dropDawn == 0 ? true : false}
+                                <input className={styles.selectBoxInput} type="radio" id="0" value="0" checked={`${dropDawn}` === `${0}` ? true : false}
                                     onChange={(e) => handleChangeDropDawn(e.target)} />
                                 <p className={styles.selectBoxInputText}>กรุณาเลือก...</p>
                                 <IconArrow />
@@ -52,10 +52,10 @@ const UploadFileComponent = () => {
                         </div>
                         <ul className={styles.selectBoxList}>
                             <li>
-                                <label className={styles.selectBoxOption} for="1" aria-hidden="aria-hidden"><i className="fas fa-check" style={{ marginRight: "10px" }}></i>ต้องการอนุมัติ</label>
+                                <label className={styles.selectBoxOption} for="1"><img src={IconCheck} width="16px" style={{marginRight: "10px"}} alt="."/>ต้องการอนุมัติ</label>
                             </li>
                             <li>
-                                <label className={styles.selectBoxOption} for="0" aria-hidden="aria-hidden">กรุณาเลือก...</label>
+                                <label className={styles.selectBoxOption} for="0">กรุณาเลือก...</label>{/* aria-hidden="aria-hidden" */}
                             </li>
                         </ul>
                     </div>
