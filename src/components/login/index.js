42ab9d28f9684a5cdaad8a876fe6afa9f5bd8b33
unknown <32980495+cl21484952@users.noreply.github.com>
2020-10-07 21:18:51 +0700

    Changelog - css update to have more consistent look

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -1,39 +1,42 @@
 import React from "react";
-import {Field, ErrorMessage} from 'formik';
+import { Field, ErrorMessage } from 'formik';
 
 import styles from './index.module.scss';
 
 const LoginComponent = () => {
     return (
-    <>
-        
+        <>
+
             <div className={styles.formControl}>
-                <p>อีเมล <ErrorMessage name="email" render={msg => <span style={{color: "red"}}>{msg}</span>}/></p>
-                <Field name="email" type="text" placeholder=""/>
+                <p>อีเมล <ErrorMessage name="email" render={msg => <span style={{ color: "red" }}>{msg}</span>} /></p>
+                <Field name="email" type="text" placeholder="" />
             </div>
 
             <div className={styles.formControl}>
-                <p>รหัสผ่าน <ErrorMessage name="password" render={msg => <span style={{color: "red"}}>{msg}</span>}/></p>
-                <Field name="password" type="text" placeholder=""/>
+                <p>รหัสผ่าน <ErrorMessage name="password" render={msg => <span style={{ color: "red" }}>{msg}</span>} /></p>
+                <Field name="password" type="text" placeholder="" />
             </div>
             <div className={styles.containerRow}>
                 <button type="submit" className={styles.buttonGreen}>เข้าสู่ระบบ</button>
                 <button className={styles.buttonBlue}>เข้าสู่ระบบด้วย Facebook</button>
                 <button className={styles.buttonLine}>เข้าสู่ระบบด้วย Line</button>
             </div>
-            <br/><br/>
+            <br />
+            <br />
             <div className={styles.containerRow}>
-                <div className={styles.containerCol}>
-                    <p className={styles.smallText}>ยังไม่เป็นสมาชิก?</p>
-                    <div className={styles.containerRow}>
+                {/* <div className={styles.containerCol}> */}
+                <p className={styles.smallText}>ยังไม่เป็นสมาชิก?</p>
+                {/* <div className={styles.containerRow}> */}
+                <div>
                     <button className={styles.buttonWhile}>สมัครสมาชิก</button>
                     <button className={styles.buttonBlue}>สมัครด้วย Facebook</button>
                     <button className={styles.buttonLine}>สมัครด้วย Line</button>
-                    </div>
                 </div>
+                {/* </div> */}
+                {/* </div> */}
             </div>
 
-    </>
+        </>
     );
 }
 export default LoginComponent;
