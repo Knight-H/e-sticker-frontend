5fd845d9e7f49f13478a36f10a4a0ef4dcbfd698
unknown <32980495+cl21484952@users.noreply.github.com>
2020-10-12 13:53:58 +0700

    Changelog - make /member-account page as in spec in figma

--- src/components/member-account/index.js
+++ src/components/member-account/index.js
@@ -78,13 +78,19 @@ const MemberAccountComponent = () => {
     return (
         <main className={styles.pageContainer}>
 
-            <h2>มุมสมาชิก - จัดการบัญชี</h2>
-            {/* <h3>สวัสดีคุณ  customer_name  เลือกเมนูการใช้งานได้เลยค่ะ</h3> */}
-            <h3>หมายเลขสมาชิก MEM0001</h3>
+
+            <div style={{ padding: "0px 0px 10px 0px" }}>
+
+                <h2>มุมสมาชิก - จัดการบัญชี</h2>
+                <h3>สวัสดีคุณ  customer_name  เลือกเมนูการใช้งานได้เลยค่ะ</h3>
+                <h3>หมายเลขสมาชิก MEM0001</h3>
+
+            </div>
 
             <div className={styles.flexWrapper}>
 
                 <EnhancedLoginCredentialsComponent email="demo@asd" emailDisabled={true} />
+                {/* <div style={{ padding: "0px 52px 0px 0px" }}></div> */}
                 <EnchancedLocationFieldsComponent onlyLocation={false} email="demo@asd" emailDisabled={true} />
 
             </div>
