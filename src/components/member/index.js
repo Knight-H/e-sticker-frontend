ff35228487980d67c62a59bd92b87e097ce9de6d
unknown <32980495+cl21484952@users.noreply.github.com>
2020-10-16 15:49:29 +0700

    Changelog - Update css of /admin/member

--- src/components/member/index.js
+++ src/components/member/index.js
@@ -2,9 +2,13 @@ import React, { useState, useEffect } from "react";
 import styles from './index.module.scss';
 import { ReactComponent as IconArrow } from './icon-arrow.svg';
 import LocationFieldsComponent from '../location-fields';
-import LoginCredentialsComponent from '../login-credentials';
+import LoginCredentialsComponent, { LoginCredentialsComponent2 } from '../login-credentials';
 import AdminKpi from '../admin-kpi';
 import { Formik, Field, Form, withFormik } from "formik";
+import { dummyHandleSubmit, dummyValidateError, DummyDiv } from "../common-scss/common"
+import auth from "../../firebase";
+import axios from 'axios'
+import { i18_th } from "../common-scss/i18_text";
 
 // const useInputChange = () => {
 //     const [input, setInput] = useState({})
@@ -17,16 +21,147 @@ import { Formik, Field, Form, withFormik } from "formik";
 //     return [input, handleInputChange]
 // }
 
-let MemberComponent = (props) => {
-    // const [selectMonth, setSelectMonth] = useInputChange();
+
+
+
+let EnhancedLoginComponent = withFormik({
+    enableReinitialize: true,
+    mapPropsToValues: (props) => {
+        return {
+            email: props.email || "",
+            password: "",
+            password_repeat: ""
+        }
+    },
+    validate: (values) => {
+        const errors = {}
+
+        if (values.email === "") {
+            errors.email = "Required!"
+        }
+
+        if (values.password !== values.password_repeat) {
+            errors.password = "Different!"
+        }
+
+        return errors
+    },
+    handleSubmit: dummyHandleSubmit
+})((props) => {
+    return (
+        <Form className={styles.loginCredentials}>
+            <LoginCredentialsComponent2 isRegistering={false} />
+        </Form>
+    )
+})
+
+let EnhancedLocationFields = withFormik({
+    enableReinitialize: true,
+    mapPropsToValues: (props) => {
+
+        return {
+            email: props.email || '',
+            phone: '',
+            address: '',
+            fullname: '',
+
+            district: '',
+            zone: '',
+            provice: '',
+            zip: '',
+
+            accountState: 0
+        }
+    },
+    validate: (values) => {
+        const errors = {}
+
+        console.log(values)
+        Object.entries(values).forEach(([fieldName, fieldValue]) => {
+            if (!fieldValue) {
+                errors[fieldName] = i18_th.required
+            }
+        })
+
+        return errors
+    },
+    handleSubmit: dummyHandleSubmit
+})((props) => {
     const [dropDawn, setDropDawn] = useState(0);
 
-    const { values } = props
+    const { values, setFieldValue } = props
 
     const handleChangeDropDawn = (e) => {
         values.accountState = parseInt(e.value) || 0 // If error value then assume be 0 (Normal)
         setDropDawn(e.value);
-    };
+    }
+
+    // useEffect(() => {
+
+    //     auth.signInWithEmailAndPassword("admin@admin.com", "admin123")
+    //     auth.onAuthStateChanged((user) => {
+    //         console.log("user", user.uid)   
+    //         axios.get("https://asia-east2-digitalwish-sticker.cloudfunctions.net/customers/" + user.uid).then((res) => {
+    //             console.log("'/customers/" + user.uid + "' got:", res.data)
+    //         })
+    //     })
+    // }, []);
+
+    // useEffect(async () => {
+
+    //     console.log("current user uid:", JSON.stringify(auth.onAuthStateChanged))
+    //     // Object.keys(values).forEach((key) => {
+    //     //     setFieldValue(key, testObj[key])
+    //     // })
+
+    // }, [])
+
+    return (
+        <Form className={styles.userInfo}>
+            <LocationFieldsComponent onlyLocation={false} />
+
+            <div className={styles.row}>
+                <p>สถานะ</p>
+                <div className={styles.selectBox}>
+
+                    <div className={styles.selectBoxCurrent} tabIndex="0">
+                        <div className={`${styles.selectBoxValue}`}>
+                            <Field className={styles.selectBoxInput} type="radio" id="0" value="0" checked={`${values.accountState}` === `${0}` ? true : false} onChange={(e) => handleChangeDropDawn(e.target)} />
+                            <p className={`${styles.selectBoxInputText}  ${styles.comboBoxStatusGreen}`}>ปกติ</p>
+                        </div>
+                        <div className={styles.selectBoxValue}>
+                            <Field className={styles.selectBoxInput} type="radio" id="1" value="1" checked={`${values.accountState}` === `${1}` ? true : false} onChange={(e) => handleChangeDropDawn(e.target)} />
+                            <p className={`${styles.selectBoxInputText}  ${styles.comboBoxStatusRed}`}>แบน</p>
+                            <IconArrow />
+                        </div>
+                    </div>
+
+                    <ul className={styles.selectBoxList}>
+                        <li><label className={`${styles.selectBoxOption} ${styles.comboBoxStatusGreen}`} for="0">ปกติ</label></li>
+                        <li><label className={`${styles.selectBoxOption} ${styles.comboBoxStatusRed}`} for="1">แบน</label></li>
+                    </ul>
+
+                </div>
+            </div>
+
+            <Field name="submit" type="submit" className={styles.greenButton} value="บันทึก" />
+        </Form>
+    )
+})
+
+let MemberComponent = (props) => {
+
+    // const emailState = useState("asdf")
+
+    // const [selectMonth, setSelectMonth] = useInputChange();
+    // const [dropDawn, setDropDawn] = useState(0);
+
+    // const { values } = props
+
+    // const handleChangeDropDawn = (e) => {
+    //     values.accountState = parseInt(e.value) || 0 // If error value then assume be 0 (Normal)
+    //     setDropDawn(e.value);
+    // };
 
     // const [accountState, setAccountState] = useState(0)
 
@@ -36,8 +171,22 @@ let MemberComponent = (props) => {
     //     const userID = urlParams.get('user_id');
     // }, []);
 
+
+
+    // const [currentEmail, setCurrentEmail] = useState("asdf@ee")
+    // const [counter, setCounter] = useState(0)
+
+    // useEffect(() => {
+    //     setInterval(()=>{
+    //         setCurrentEmail("kkkkkk@rtyu"+counter)
+    //         setCounter(counter + 1)
+    //     })
+    //     setFieldValue
+    // }, [currentEmail])
+    const currentEmail = "123@123.123"
+
     return (
-        <main className={styles.container}>
+        <main className={styles.pageContainer}>
 
             <section className={styles.section1}>
                 <AdminKpi kpi={{ "order": 10, "sales": 1234567, "member": 1000 }} />
@@ -50,49 +199,15 @@ let MemberComponent = (props) => {
 
                 <div className={styles.flexWrapper}>
 
-                    <section className={styles.loginCredentials}>
-                        <LoginCredentialsComponent isRegistering={false} />
-                    </section>
-
-                    <section className={styles.userInfo}>
-                        <Form>
-                            <LocationFieldsComponent onlyLocation={false} />
-
-                            <div className={styles.row}>
-                                <p>สถานะ</p>
-                                <div className={styles.selectBox}>
-
-                                    {/* Display option when dropdown is hidden */}
-                                    <div className={styles.selectBoxCurrent} tabIndex="0">
-                                        <div className={`${styles.selectBoxValue}`}>
-                                            <Field className={styles.selectBoxInput} type="radio" id="0" value="0" checked={`${values.accountState}` === `${0}` ? true : false} onChange={(e) => handleChangeDropDawn(e.target)} />
-                                            <p className={`${styles.selectBoxInputText}  ${styles.comboBoxStatusGreen}`}>ปกติ</p>
-                                        </div>
-                                        <div className={styles.selectBoxValue}>
-                                            <Field className={styles.selectBoxInput} type="radio" id="1" value="1" checked={`${values.accountState}` === `${1}` ? true : false} onChange={(e) => handleChangeDropDawn(e.target)} />
-                                            <p className={`${styles.selectBoxInputText}  ${styles.comboBoxStatusRed}`}>แบน</p>
-                                            <IconArrow />
-                                        </div>
-                                    </div>
-
-                                    {/* The dropdown options */}
-                                    <ul className={styles.selectBoxList}>
-                                        <li><label className={`${styles.selectBoxOption} ${styles.comboBoxStatusGreen}`} for="0">ปกติ</label></li>
-                                        <li><label className={`${styles.selectBoxOption} ${styles.comboBoxStatusRed}`} for="1">แบน</label></li>
-                                    </ul>
-
-                                </div>
-                            </div>
-
-                            <Field type="submit" className={styles.greenButton} value="บันทึก" />
-                        </Form>
-                    </section>
+                    <EnhancedLoginComponent email={currentEmail} emailDisabled={true} />
+                    <EnhancedLocationFields email={currentEmail} emailDisabled={true} />
 
                 </div>
             </section>
         </main>
     )
 }
+
 
 const EnhancedMemberComponent = withFormik({
     mapPropsToValues: () => ({
@@ -123,5 +238,8 @@ const EnhancedMemberComponent = withFormik({
     }
 })(MemberComponent)
 
-MemberComponent = EnhancedMemberComponent
-export default MemberComponent;
+// MemberComponent = EnhancedMemberComponent
+// let memberComponent = MemberComponent
+
+
+export default (props) => { return (<MemberComponent />) }
